92c4e83a94f2140df80b9d293c4fb4b8e98a3b77
Prateek Soni <prateek_soni@outlook.com>
2023-01-16 06:04:41 +0530

    add learn posts

---
+++ src/pages/dashboard/learn/roadmap.md
@@ -0,0 +1,19 @@
+---
+layout: "../../../layouts/LearnLayout.astro"
+title: 'Roadmap'
+---
+
+# Roadmap
+
+This page will guide you through the journey of becoming a full stack web developer by outlining the roadmap for this series.
+
+- Introduction to Web
+- HTML
+- CSS
+- Javascript
+- Data structures
+- Algorithms
+- SQL
+- React
+- NodeJS
+- Meta Frameworks
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+    },
+  },
+
+  plugins: [require("@tailwindcss/typography")],
+};
